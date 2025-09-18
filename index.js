76e475145bd58771c397aae5b541f851ad354876
Amarante Wellington Freire Da Silva <amaranthwfs@gmail.com>
2025-09-18 10:13:05 -0300

    feat: add route to create a new task

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const connectToDatabase = require("./src/database/mongoose.database.js");
+const taskModel = require("./src/models/task.models.js");
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 connectToDatabase();
 
-app.get("/", (req, res) => {
-    const task = [{ description: "Estudar programação", isCompleted: false }];
-    res.status(200).send(task);
+app.get("/task", async (req, res) => {
+    try {
+        const task = await taskModel.find({});
+        res.status(200).send(task);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post("/task", async (req, res) => {
+    try {
+        const newTask = new taskModel(req.body);
+        await newTask.save();
+        res.status(201).send(newTask);
+    } catch (error) {
+        res.status(500).send(error.messager);
+    }
 });
 app.listen(8000, () => console.log("Listening on port 8000"));
--- src/models/task.models.js
+++ src/models/task.models.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 const taskSchema = Schema({
     description: {
-        type: string,
+        type: String,
         require: true,
     },
     isCompleted: {
