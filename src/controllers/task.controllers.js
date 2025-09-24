159269df936f5671b01b746993b6a599a9eb1b9c
Amarante Wellington Freire Da Silva <amaranthwfs@gmail.com>
2025-09-23 22:45:25 -0300

    feat: move get by id  tasks endpoint to a controller

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -12,5 +12,19 @@ class TaskController {
             this.res.status(500).send(error.message);
         }
     }
+    async getById() {
+        try {
+            const taskId = this.req.params.id;
+            const task = await taskModel.findById(taskId);
+            if (!task) {
+                return this.res
+                    .status(404)
+                    .send("Task não encontrada ou já deletada!");
+            }
+            return this.res.status(200).send(task);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
 }
 module.exports = TaskController;
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -9,16 +9,7 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const task = await taskModel.findById(taskId);
-        if (!task) {
-            return res.status(404).send("Task não encontrada ou já deletada!");
-        }
-        return res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).getById();
 });
 
 router.post("/", async (req, res) => {
